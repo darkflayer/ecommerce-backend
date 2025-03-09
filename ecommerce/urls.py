cf5b2919810280e03cd6f9d3a3e97aace3fc578c
Hemant <rautanhemu@gmail.com>
2025-03-10 00:51:02 +0530

    home-page-fixed

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello! Your Django App is running.")
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
+     path('', home),
 ]
 
